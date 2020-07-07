4f4cd8f56e44a039307a0dd9fc8623ef72cc9618
Mochamad Aufa <mochamadaufa7@gmail.com>
2020-07-07 22:22:28 +0700

    [07/july/2020] test: getDataYoutube: add duration

--- test/test.js
+++ test/test.js
@@ -6,6 +6,18 @@ module.exports = {
         var dataVideo = []
         var {data} = req.query
 
+        const duration = (duration) => {
+            var match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/)
+            !match ? match = duration.match(/P(\d+DT)?(\d+H)?(\d+M)?(\d+S)?/)
+                : match.splice(1, 0, undefined)
+            return {
+                day: parseInt(match[1] || 0),
+                hour: parseInt(match[2] || 0),
+                minute: parseInt(match[3] || 0),
+                second: parseInt(match[4] || 0)
+            }
+        }
+
         const ytV3 = google.youtube({version: 'v3', auth: process.env.API_KEY})
         const search = await ytV3.search.list({part: 'snippet', maxResults: 5, q: data})
         search.data.items.forEach(x => {
@@ -16,11 +28,9 @@ module.exports = {
             dataVideo.push({
                 title: x.snippet.title,
                 channel: x.snippet.channelTitle,
-                duration: x.contentDetails.duration
+                duration: duration(x.contentDetails.duration)
             })
         })
-        
-        console.log(dataVideo)
 
         return res.status(200).json({
             data: dataVideo
